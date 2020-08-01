9a2176d51120e3753e28ddebcd98d9ea4322f771
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-08-01 18:02:43 +0400

    Added Layout

--- routes/admin.js
+++ routes/admin.js
@@ -11,7 +11,10 @@ const products = [];
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
 	//res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-	res.render('add-product', { docTitle: 'Add Product test' });
+	res.render('add-product', {
+		docTitle: 'Add Product test',
+		path: '/admin/add-product',
+	});
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,11 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
 	console.log('shop.js', adminData.products);
 	//res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-	res.render('shop', { prods: adminData.products, docTitle: 'Shop' });
+	res.render('shop', {
+		prods: adminData.products,
+		docTitle: 'Shop',
+		path: '/',
+	});
 });
 
 module.exports = router;
