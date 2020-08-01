d4d7db019274a61f9ee6a4c44679cf2865252514
Vaskar Sarma <vaskar.sarma@emirates.com>
2020-08-01 17:42:17 +0400

    Updated code to use PUG as the default template engine

--- routes/shop.js
+++ routes/shop.js
@@ -1,15 +1,16 @@
-const path = require('path');
+//const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
+//const rootDir = require('../utils/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
 	console.log('shop.js', adminData.products);
-	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	//res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+	res.render('shop', { prods: adminData.products, docTitle: 'Shop' });
 });
 
 module.exports = router;
